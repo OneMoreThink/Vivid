04716851315899a1d44d59debc81b167c7ab17b1
fine <92whdtjs@gmail.com>
2025-08-21 12:09:21 +0900

    Add Firebase Crashlytics dependency

---
+++ Vivid/Vivid/App/AppDelegate.swift
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.swift
+//  Vivid
+//
+//  Created by 이종선 on 8/21/25.
+//
+
+import SwiftUI
+import FirebaseCore
+
+
+class AppDelegate: NSObject, UIApplicationDelegate {
+  func application(_ application: UIApplication,
+                   didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+    FirebaseApp.configure()
+
+    return true
+  }
+}
