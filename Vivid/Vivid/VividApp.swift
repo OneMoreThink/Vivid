955e68427865916bccf5d7de7d9ba7cc2cbc52b2
fine <92whdtjs@gmail.com>
2025-08-21 11:56:38 +0900

    Init Xcode Project

---
+++ Vivid/Vivid/VividApp.swift
@@ -0,0 +1,17 @@
+//
+//  VividApp.swift
+//  Vivid
+//
+//  Created by 이종선 on 8/21/25.
+//
+
+import SwiftUI
+
+@main
+struct VividApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
