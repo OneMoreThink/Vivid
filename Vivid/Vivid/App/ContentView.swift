b13ae6b4ce9548c43af87d17b7d7f5d60109b2f3
fine <92whdtjs@gmail.com>
2025-09-16 15:30:48 +0900

    Firebase Analytics 추가

--- Vivid/Vivid/App/ContentView.swift
+++ Vivid/Vivid/App/ContentView.swift
@@ -6,13 +6,18 @@
 //
 
 import SwiftUI
+import FirebaseAnalytics
 
 struct ContentView: View {
     var body: some View {
         VStack {
             Button("Crash") {
               fatalError("Crash was triggered")
-            } 
+            }
+            
+            Button("Logging") {
+                Analytics.logEvent("timeline", parameters: nil)
+            }
         }
         .padding()
     }
